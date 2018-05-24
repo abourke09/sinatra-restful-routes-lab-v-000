c93e30a0c35c87f192869136cfbfda1ad9d1506d
Annie Bourke <aabourke@gmail.com>
2018-05-24 22:59:12 +0000

    automatically backed up by learn

--- db/migrate/20180524225732_create_recipes.rb
+++ db/migrate/20180524225732_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
       create_table :recipes do |t|
         t.string :name
         t.string :ingredients
-        t.decimal :cooktime
+        t.integer :cooktime
       end
     end
 end
